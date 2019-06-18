8f3f6d63baf5abc61fd7262d89995b4b208f5d18
e858304 <e858304@WPMESQ6CC4915.central.rinterna.local>
2019-06-18 12:29:30 +0100

    Add commands

--- src/main/java/br/com/zukeran/tiojiro/telegrambot/service/TioTelegramBotServiceImpl.java
+++ src/main/java/br/com/zukeran/tiojiro/telegrambot/service/TioTelegramBotServiceImpl.java
@@ -17,50 +17,93 @@ import com.pengrad.telegrambot.response.BaseResponse;
 import com.pengrad.telegrambot.response.GetMeResponse;
 import com.pengrad.telegrambot.response.SendResponse;
 
-import br.com.zukeran.tiojiro.telegrambot.config.TioTelegramBotConfig;
-
 @Service
 public class TioTelegramBotServiceImpl implements TioTelegramBotService{
 	
 	private static final String VAZIO = "";
+	private static final String SPACE = " ";
+	private static final String CMD = "/";
+	private static final String CMD_START = "/start";
+	private static final String CMD_IMG = "/img";
 	
 	@Autowired
-	private TioTelegramBotConfig tioBotConfig;
+	TelegramBot bot;
 
 	@Override
 	public boolean webhook(String strUpdate) throws Exception {
-		SendResponse sendResponse;
-		BaseResponse baseResponse;
-		
-		TelegramBot bot = tioBotConfig.telegramBot();
+		boolean ret = false;
 		Update update = BotUtils.parseUpdate(strUpdate);
+		String command = getCommand(update);
 		
-		System.out.println("Mensagem recebida: ["+ update.message().text() + "]");
+		switch(command) {
+			case CMD_START:
+				ret = sendHelloMessage(update);
+				break;
+			case CMD_IMG:
+				ret = sendHelloMessage(update);
+				break;
+		}
 		
-		baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-		System.out.println("Chat Action enviada? [" + baseResponse.isOk() + "]");
-		
-		TimeUnit.SECONDS.sleep(1);
-		
-		if(!VAZIO.equals(update.message().from().username()))
-			sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Olá, " + update.message().from().username() + "!"));
-		else if (!VAZIO.equals(update.message().from().firstName()))
-			sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Olá, " + update.message().from().firstName() + "!"));
-		else
-			sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Olá!"));
-		System.out.println("Mensagem enviada? [" +sendResponse.isOk() + "]");
-		
-		return sendResponse.isOk();
+		return ret;
 	}
 
 	@Override
 	public User getMe() {
-		GetMeResponse getMeResponse;
-		TelegramBot bot = tioBotConfig.telegramBot();
-		
-		getMeResponse = bot.execute(new GetMe());
-
+		GetMeResponse getMeResponse = bot.execute(new GetMe());
 		return getMeResponse.user();
+	}
+	
+	private String getCommand(Update update) {
+		String msg = update.message().text();
+		if(msg.startsWith(CMD))
+			return msg.split(SPACE)[0];
+		else
+			return VAZIO;
+	}
+	
+	private String getFrom(Update update) {
+		String from = null;
+		String userName = update.message().from().username();
+		String firstName =  update.message().from().firstName();
+		String id = update.message().from().id().toString();
 		
+		if (firstName != null && !VAZIO.equals(firstName))
+			from = "firsName: " + firstName;
+		else if(userName != null && !VAZIO.equals(userName))
+			from = "userName: " + userName;
+		else if(id != null && !VAZIO.equals(id))
+			from = "id: " + id;
+		
+		System.out.println("Update received from: ["+ from + "]");
+		
+		return from;
+	}
+	
+	private boolean sendHelloMessage (Update update) throws InterruptedException {
+		boolean ret = true;
+		SendResponse sendResponse;
+		String from = getFrom(update);
+		
+		if(sendTyping(update)) {
+			if(from != null)
+				sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Olá, " + from + "!"));
+			else
+				sendResponse = bot.execute(new SendMessage(update.message().chat().id(),"Olá!"));
+			
+			ret = sendResponse.isOk();
+		} else {
+			ret = false;
+		}
+		
+		return ret;
+	}
+	
+	private boolean sendTyping(Update update) throws InterruptedException {
+		BaseResponse baseResponse;
+		baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+
+		TimeUnit.SECONDS.sleep(1);
+		
+		return baseResponse.isOk();
 	}
 }
